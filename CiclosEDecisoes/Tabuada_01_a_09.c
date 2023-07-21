280e05f713f4842636adb3bb016ccecc4bf44701
Maria <conceicao.martinho.estudo@gmail.com>
2023-07-21 12:28:49 +0100

    Update Tabuada_01_a_09.c

--- CiclosEDecisoes/Tabuada_01_a_09.c
+++ CiclosEDecisoes/Tabuada_01_a_09.c
@@ -5,6 +5,8 @@ int main()
     int contador, i;
     for (i = 1; i < 10; i++)
     {
+        printf("Tabuada do %d", i);
+        printf("\n");
         for ( contador = 1; contador < 10;  contador ++)
         {
             printf("%d X % d =  %d \n", i, contador, i*contador);
