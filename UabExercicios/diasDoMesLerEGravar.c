59ef5dda557c705a78fa6eda83220e85718b5411
Maria <conceicao.martinho.estudo@gmail.com>
2024-01-08 20:56:20 +0000

    Atualização

---
+++ UabExercicios/diasDoMesLerEGravar.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+    int ano, mes;
+    FILE *f, *f2;
+    f=fopen("in.txt", "rt");
+    f2= fopen("out.txt", "wt");
+
+    if (f==NULL || f2==NULL)
+    {
+        return 0;
+    }
+
+    fscanf(f, "%d", &ano);
+    fscanf(f, "%d", &mes);
+
+    fclose(f);
+
+    if(mes==2)
+    {
+        if(ano%400==0 || ano%4==0 && ano%100!=0)
+        {
+            fprintf(f2, "29");
+        }
+            
+        else
+        {
+            fprintf(f2, "28");
+        }
+            
+        
+    }
+
+    else if(mes==1 || mes==3 || mes==5 || mes==7 || mes==8 || mes==10 || mes==12)
+    {
+        fprintf(f2, "31");
+    }
+    else
+    {
+        fprintf(f2, "30");
+    }
+    fclose(f2);
+
+}
